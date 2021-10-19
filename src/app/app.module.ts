b21bfed23ceecb7089de548610ab7311335baaf0
stannarshosay <stannarsjose@gmail.com>
2021-10-19 12:34:36 +0530

    dealers component added and base created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,12 +34,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DealersComponent } from './components/dealers/dealers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    DealersComponent
   ],
   imports: [
     BrowserModule,
